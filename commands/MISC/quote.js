6fce73debc2926ce00287dfc1a0df9d94d36a1a2
NotJishy <jishydoods@gmail.com>
2020-10-19 17:31:19 -0400

    Fixed quote command

--- commands/MISC/quote.js
+++ commands/MISC/quote.js
@@ -1,63 +1,70 @@
 exports.run = function (Discord, msg, args) {
   const fs = require('fs')
-  const profiles = require('../assets/quotes/char-profiles.json')
-
-  var character
-  fs.readdir('./assets/quotes/characters', { encoding: 'utf8' }, function (err, chars) {
+  //const profiles = require('../assets/quotes/char-profiles.json')
+  fs.readFile(`./assets/quotes/char-profiles.json`, { encoding: 'utf8' }, function (err, data) {
     if (err) {
-      console.log(new Date() + ` - ${err}`)
+      return console.log(new Date() + ` - ${err}`)
     } else {
-      var count = 0
-      chars.forEach(char => {
-        if (args[0]) {
-          if (args[0].toLowerCase() == char) {
-            character = char
-            count++
-          }
-        }
-      })
+      const profiles = JSON.parse(data)
 
-      if (count == 0) {
-        character = chars[Math.floor(Math.random() * chars.length)]
-      }
-
-      fs.readdir(`./assets/quotes/characters/${character}`, { encoding: 'utf8' }, function (err, dataa) {
+      var character
+      fs.readdir('./assets/quotes/characters', { encoding: 'utf8' }, function (err, chars) {
         if (err) {
-          return console.log(new Date() + ` - ${err}`)
+          console.log(new Date() + ` - ${err}`)
         } else {
-          let quote = dataa[Math.floor(Math.random() * dataa.length)]
-
-          quote = fs.readFile(`./assets/quotes/characters/${character}/${quote}`, { encoding: 'utf8' }, function (err, datab) {
-            if (err) {
-              console.log(new Date() + ` - ${err}`)
-            } else {
-              quote = JSON.parse(datab)
-
-              var charProfile
-              switch (character) {
-                case 'elsa':
-                  charProfile = profiles.elsa
-                  break;
-                case 'anna':
-                  charProfile = profiles.anna
-                  break;
-                case 'kristoff':
-                  charProfile = profiles.kristoff
-                  break;
-                case 'olaf':
-                  charProfile = profiles.olaf
-                  break;
-                case 'pabbie':
-                  charProfile = profiles.pabbie
-                  break;
+          var count = 0
+          chars.forEach(char => {
+            if (args[0]) {
+              if (args[0].toLowerCase() == char) {
+                character = char
+                count++
               }
+            }
+          })
 
-              const embed = new Discord.MessageEmbed()
-                .setAuthor(character.charAt(0).toUpperCase() + character.substring(1), charProfile.icon)
-                .setTitle(`"${quote.text}"`)
-                .setFooter(quote.origin)
-                .setColor(charProfile.color)
-              msg.channel.send(embed)
+          if (count == 0) {
+            character = chars[Math.floor(Math.random() * chars.length)]
+          }
+
+          fs.readdir(`./assets/quotes/characters/${character}`, { encoding: 'utf8' }, function (err, dataa) {
+            if (err) {
+              return console.log(new Date() + ` - ${err}`)
+            } else {
+              let quote = dataa[Math.floor(Math.random() * dataa.length)]
+
+              quote = fs.readFile(`./assets/quotes/characters/${character}/${quote}`, { encoding: 'utf8' }, function (err, datab) {
+                if (err) {
+                  console.log(new Date() + ` - ${err}`)
+                } else {
+                  quote = JSON.parse(datab)
+
+                  var charProfile = null
+                  switch (character) {
+                    case 'elsa':
+                      charProfile = profiles.elsa
+                      break;
+                    case 'anna':
+                      charProfile = profiles.anna
+                      break;
+                    case 'kristoff':
+                      charProfile = profiles.kristoff
+                      break;
+                    case 'olaf':
+                      charProfile = profiles.olaf
+                      break;
+                    case 'pabbie':
+                      charProfile = profiles.pabbie
+                      break;
+                  }
+
+                  const embed = new Discord.MessageEmbed()
+                    .setAuthor(character.charAt(0).toUpperCase() + character.substring(1), charProfile.icon)
+                    .setTitle(`"${quote.text}"`)
+                    .setFooter(quote.origin)
+                    .setColor(charProfile.color)
+                  msg.channel.send(embed)
+                }
+              })
             }
           })
         }
