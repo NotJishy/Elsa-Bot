a2888e750bc864324b02d94798dd41f2644bbf74
NotJishy <jishydoods@gmail.com>
2020-10-21 12:38:21 -0400

    snowball command now has a gif of Anna throwing a snowball, this gif is also added to the Anna command

--- commands/FUN/snowball.js
+++ commands/FUN/snowball.js
@@ -2,7 +2,11 @@ exports.run = function(Discord, msg) {
   if (msg.mentions.members.first()) {
     const messages = ["{victim} was snowballed in the face by {criminal}!", "{criminal} has pelted {victim} in the face with a snowball!", "{victim} was thrown into the void with a snowball by {criminal}"]
 
+    const attachment = new Discord.MessageAttachment(`./assets/images/anna/snowball.gif`)
+
     const embed = new Discord.MessageEmbed()
+      .attachFiles(attachment)
+      .setImage(`attachment://snowball.gif`)
       .setColor("#E1E1E1")
       .setDescription(messages[Math.floor(Math.random() * messages.length)].replace('{victim}', msg.mentions.members.first()).replace('{criminal}', msg.author))
 
